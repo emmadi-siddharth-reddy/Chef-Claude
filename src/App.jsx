c463ea1d226bcf9fb17363a059bdf8770c332c98
Emmadi Siddharth <emmadisiddharthreddy@gmail.com>
2025-06-30 22:07:08 +0530

    move everything into specific component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Header } from "./Components/Header"
 import { Main } from "./Components/Main"
-import { Practice } from "./Practice/Practice"
+
 
 
 export function App() {
   return (
     <>
-    {/* < Practice /> */}
+
     <Header />
     <Main />
     
