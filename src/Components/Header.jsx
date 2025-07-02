a686c8a0a9faae50291b31e3a74daa36003570ee
Emmadi Siddharth <emmadisiddharthreddy@gmail.com>
2025-07-02 21:14:35 +0530

    update image path

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
     return (
         <header>
-            <img src="images/chef-claude-icon.png" alt="Icon Image" />
+            <img src="/images/chef-claude-icon.png" alt="Icon Image" />
             <h1>Chef AI</h1>
         </header>
     )
